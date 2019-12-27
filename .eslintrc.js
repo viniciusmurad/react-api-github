5529e806d674d63c6ea9bb9dd286ab4ac264bf48
Vinicius <vm.murad@gmail.com>
2019-12-27 01:13:23 -0300

    added axios and loading animation btn

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
   ],
   rules: {
       'prettier/prettier' : 'error',
+      'react/state-in-constructor': 0,
       'react/jsx-filename-extension': [
           'warn',
           { extensions: ['.jsx', '.js'] }
